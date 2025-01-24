debee5a8dc4c990d3d0f556ac8608003a7388742
Peiwen Lu <hi@peiwen.lu>
2025-01-24 08:25:24 +0800

    feat: switch to eslint flat config format

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  { ignores: ["dist", "node_modules", ".git", ".vite"] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.tsx"],
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    languageOptions: {
+      parserOptions: {
+        project: "tsconfig.json"
+      }
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    extends: [
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"],
+      jsxA11y.flatConfigs.recommended
+    ],
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
+    }
+  },
+  {},
+  eslintPluginPrettierRecommended
+);
