d6daf20d7249c7f90f7c411671f51e39cce5168b
Peiwen Lu <hi@peiwen.lu>
2025-01-24 18:29:04 +0800

    fix: replace browser router with hash router

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,30 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "~/styles/global.scss";
 
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/about",
-        element: <About />
-      }
-    ]
-  }
-]);
-
 createRoot(document.body).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
