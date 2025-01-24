56aef9d56604ee209393ab4bb59d8ec835995747
Peiwen Lu <hi@peiwen.lu>
2025-01-24 19:58:28 +0800

    feat: open devtool in development only

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,18 +14,18 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js")
+      // ,devTools: !app.isPackaged // disable devTools in production
     }
   });
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    // Open devtool in development only
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
